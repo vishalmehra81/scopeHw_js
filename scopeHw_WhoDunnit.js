ba74d19c49fa39f4cda2e86c59805f21f9a5dd8a
Vishal Mehra <67109899+vishalmehra81@users.noreply.github.com>
2021-02-02 17:12:35 +0000

    2nd Episode

--- scopeHw_WhoDunnit.js
+++ scopeHw_WhoDunnit.js
@@ -11,4 +11,21 @@ const scenario = {
   const verdict = declareMurderer();
   console.log(verdict);
 
-//* The murderer is Miss Scarlet
+//TODO The murderer is Miss Scarlet
+
+const murderer = 'Professor Plum';
+
+const changeMurderer = function() {
+  murderer = 'Mrs. Peacock';
+}
+
+const declareMurderer = function() {
+  return `The murderer is ${murderer}.`;
+}
+
+changeMurderer();
+const verdict = declareMurderer();
+console.log(verdict);
+
+//TODO Error - The variable murderer is assigned with const keyword and it cannot be reassigned. When the
+//TODO changeMurderer functions is called, it tries changing the variable murderer and results in error.
