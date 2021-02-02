d2d66d5685d3a45bd1ac1fe406c5654be1633b90
Vishal Mehra <67109899+vishalmehra81@users.noreply.github.com>
2021-02-02 15:59:50 +0000

    First Episode

---
+++ scopeHw_WhoDunnit.js
@@ -0,0 +1,14 @@
+const scenario = {
+    murderer: 'Miss Scarlet',
+    room: 'Library',
+    weapon: 'Rope'
+  };
+  
+  const declareMurderer = function() {
+    return `The murderer is ${scenario.murderer}.`;
+  }
+  
+  const verdict = declareMurderer();
+  console.log(verdict);
+
+//* The murderer is Miss Scarlet
